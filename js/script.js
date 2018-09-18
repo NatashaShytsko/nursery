8cb65836633adb5f804278a76a6ea08f28430044
Natasha Shytsko <natashashitko@gmail.com>
2018-09-18 17:26:48 +0300

    Fixed for mobile

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('span.navbar-toggle').click(function (e) {
+        $(this).toggleClass('active');
+        $('nav').toggle();
+        e.stopPropagation();
+    });
+    $('body').click(function () {
+        var link = $('span.navbar-toggle');
+        if (link.hasClass('active')) {
+            link.click();
+        }
+    });
+});
